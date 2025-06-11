998948fb9b4745822229f250cc8de6aee53c39a5
aniket1251 <ag125aa@gmail.com>
2025-06-11 08:39:04 +0530

    fix api

--- utils/fetchNews.ts
+++ utils/fetchNews.ts
@@ -1,7 +1,38 @@
+// import axios from 'axios';
+
+// export async function fetchNews(query = '') {
+//   const url = `https://newsapi.org/v2/everything?q=${query || 'tech'}&apiKey=5d0262e362104b8188a31e610ea309bb`;
+//   const { data } = await axios.get(url);
+//   return data.articles;
+// }
+
+
 import axios from 'axios';
 
-export async function fetchNews(query = '') {
-  const url = `https://newsapi.org/v2/everything?q=${query || 'tech'}&apiKey=5d0262e362104b8188a31e610ea309bb`;
-  const { data } = await axios.get(url);
-  return data.articles;
-}
+export const fetchNews = async () => {
+  try {
+    const apiKey = "eb4c00650acd6ec4469ed71c8cbe17af";
+    const response = await axios.get('https://gnews.io/api/v4/search', {
+      params: {
+        q: 'technology',
+        token: apiKey,
+        lang: 'en',
+        max: 50,
+      },
+    });
+
+    // Transform GNews format to expected format
+    return response.data.articles.map((article: any) => ({
+      title: article.title,
+      description: article.description,
+      url: article.url,
+      imageUrl: article.image,
+      publishedAt: article.publishedAt,
+      author: article.source.name,
+      type: 'news',
+    }));
+  } catch (error) {
+    console.error('Error fetching GNews articles:', error);
+    return [];
+  }
+};
